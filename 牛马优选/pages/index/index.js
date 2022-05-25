05ec0feaf38f77a457bc810da571ad34b706a0db
酷酷的标 <105979528+kukudebiao@users.noreply.github.com>
2022-05-25 11:09:59 +0800

    Add files via upload

---
+++ 牛马优选/pages/index/index.js
@@ -0,0 +1,74 @@
+// pages/classify/classify.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    img: [
+      'https://img.zcool.cn/community/01224e5d637723a8012187f442586c.jpg?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/format,webp/quality,Q_100',
+     'https://img.zcool.cn/community/01b1c85d63772ea8012187f4648636.jpg?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/format,webp/quality,Q_100', 
+     'https://img.zcool.cn/community/01c3de5d637733a8012187f4c62168.jpg?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/format,webp/quality,Q_100'],
+     indicatorDots: true,
+     vertical: false,
+     autoplay: true,
+     interval: 2000,
+     duration: 500,
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
